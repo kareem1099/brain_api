8a70aa146a1910984572b00aac1a5ee06483e5cb
Kareem <117325791+kareem1099@users.noreply.github.com>
2025-05-18 11:29:38 +0300

    ه

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 import numpy as np
-import cv2
 import shutil
 import os
+import tensorflow as tf
 
 app = FastAPI()
 
@@ -12,9 +13,10 @@ model = load_model("models/model_image_(Brain).h5")
 brain_classes = ['brain_menin', 'brain_glioma', 'brain_pituitary', 'no_tumor']
 
 def preprocess_brain_image(path):
-    img = cv2.imread(path)
-    img = cv2.resize(img, (128, 128)) / 255.0
-    return np.expand_dims(img, axis=0)
+    img = tf.keras.utils.load_img(path, target_size=(128, 128))
+    img_array = tf.keras.utils.img_to_array(img)
+    img_array = img_array / 255.0
+    return np.expand_dims(img_array, axis=0)
 
 @app.post("/predict_Brain")
 async def predict_brain(file: UploadFile = File(...)):
@@ -31,6 +33,8 @@ async def predict_brain(file: UploadFile = File(...)):
         return {"result": predicted_class}
     except Exception as e:
         return JSONResponse({"error": str(e)})
+
 if __name__ == "__main__":
+    import uvicorn
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port, reload=False)
